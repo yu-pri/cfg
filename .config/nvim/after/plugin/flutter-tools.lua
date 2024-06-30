a71485b7c22dd570089a92f1c554f5ebdc6e7b55
Yurii Prykhodko <yuri.pryhodko@gmail.com>
2024-06-30 14:52:52 +0300

    dynamic home

--- .config/nvim/after/plugin/flutter-tools.lua
+++ .config/nvim/after/plugin/flutter-tools.lua
@@ -1,10 +1,10 @@
 require("flutter-tools").setup {
-    flutter_path = '/home/yupri/fvm/versions/stable/bin/flutter',
+    flutter_path = vim.fn.expand('$HOME/fvm/versions/stable/bin/flutter'),
     lsp = {
         settings = {
             analysisExcludedFolders = {
-                "/home/yupri/.pub-cache/",
-                '/home/yupri/fvm/versions/stable/',
+                vim.fn.expand("$HOME/.pub-cache/"),
+                vim.fn.expand('$HOME/fvm/versions/stable/'),
             }
         }
     }
